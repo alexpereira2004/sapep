4e8ad5c09529a1128d861b148f88c61c89715d3c
alexpereira2017 <alexpereira2004@gmail.com>
2019-07-15 22:15:06 -0300

    Commit inicial

---
+++ src/main/java/br/com/lunacom/sapep/SapepApplication.java
@@ -0,0 +1,13 @@
+package br.com.lunacom.sapep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SapepApplication.class, args);
+    }
+
+}
