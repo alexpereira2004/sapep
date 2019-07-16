e84b8099d69392895ebc9b25d7e266b33252ef8d
alexpereira2017 <alexpereira2004@gmail.com>
2019-07-16 00:39:06 -0300

    [Issue #2] Feature

--- src/main/java/br/com/lunacom/sapep/domain/Responsavel.java
+++ src/main/java/br/com/lunacom/sapep/domain/Responsavel.java
@@ -2,6 +2,6 @@ package br.com.lunacom.sapep.domain;
 
 import java.io.Serializable;
 
-public class Responsavel  implements Serializable {
+public class Responsavel implements Serializable {
     private static final long serialVersionUID = 1L;
 }
