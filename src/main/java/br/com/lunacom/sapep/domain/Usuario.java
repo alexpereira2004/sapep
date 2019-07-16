842df69b123aba47acc7e2e55c688f8aba6e161a
alexpereira2017 <alexpereira2004@gmail.com>
2019-07-16 00:31:38 -0300

    [#2] Feature

--- src/main/java/br/com/lunacom/sapep/domain/Usuario.java
+++ src/main/java/br/com/lunacom/sapep/domain/Usuario.java
@@ -1,4 +1,7 @@
 package br.com.lunacom.sapep.domain;
 
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
 }
