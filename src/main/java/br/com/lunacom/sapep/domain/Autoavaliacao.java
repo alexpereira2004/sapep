6f9217db8c36060a4615d29a9d7a8229d5090048
alexpereira2017 <alexpereira2004@gmail.com>
2019-07-22 21:50:51 -0300

    [Issue #2] Feature - Revisão dos relecionamentos das Autoavaliacao e Eixo

--- src/main/java/br/com/lunacom/sapep/domain/Autoavaliacao.java
+++ src/main/java/br/com/lunacom/sapep/domain/Autoavaliacao.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Time;
-import java.util.Date;
+import java.util.*;
 
 @Entity
 public class Autoavaliacao implements Serializable {
@@ -27,4 +27,120 @@ public class Autoavaliacao implements Serializable {
     @JoinColumn(name="curso_id")
     private Curso curso;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "id.autoavaliacao")
+    private Set<AutoavaliacaoEixos> autoavaliacaoEixos = new HashSet<>();
+
+    public Autoavaliacao() {}
+
+    public Autoavaliacao(String nome, String objetivo, String status, Date inicio, Date termino) {
+        this.nome = nome;
+        this.objetivo = objetivo;
+        Status = status;
+        this.inicio = inicio;
+        this.termino = termino;
+    }
+
+    @JsonIgnore
+    public List<Eixo> getEixos() {
+        List<Eixo> lista = new ArrayList<>();
+        for (AutoavaliacaoEixos x : autoavaliacaoEixos) {
+            lista.add(x.getEixo());
+        }
+        return lista;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(String objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public Date getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Date inicio) {
+        this.inicio = inicio;
+    }
+
+    public Date getTermino() {
+        return termino;
+    }
+
+    public void setTermino(Date termino) {
+        this.termino = termino;
+    }
+
+    public Date getCriacao() {
+        return Criacao;
+    }
+
+    public void setCriacao(Date criacao) {
+        Criacao = criacao;
+    }
+
+    public Time getHora() {
+        return Hora;
+    }
+
+    public void setHora(Time hora) {
+        Hora = hora;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Set<AutoavaliacaoEixos> getAutoavaliacaoEixos() {
+        return autoavaliacaoEixos;
+    }
+
+    public void setAutoavaliacaoEixos(Set<AutoavaliacaoEixos> autoavaliacaoEixos) {
+        this.autoavaliacaoEixos = autoavaliacaoEixos;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
--- src/main/java/br/com/lunacom/sapep/domain/Eixo.java
+++ src/main/java/br/com/lunacom/sapep/domain/Eixo.java
@@ -1,10 +1,14 @@
 package br.com.lunacom.sapep.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Time;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Eixo implements Serializable {
@@ -18,6 +22,92 @@ public class Eixo implements Serializable {
     private Date criacao;
     private Time hora;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "id.eixo")
+    private Set<AutoavaliacaoEixos> autoavaliacaoEixos = new HashSet<>();
+
     @OneToMany(mappedBy = "eixo")
     private List<Indicador> indicadores;
+
+    public Eixo() {}
+
+    public Eixo(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getCriacao() {
+        return criacao;
+    }
+
+    public void setCriacao(Date criacao) {
+        this.criacao = criacao;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+
+    public Set<AutoavaliacaoEixos> getAutoavaliacaoEixos() {
+        return autoavaliacaoEixos;
+    }
+
+    public void setAutoavaliacaoEixos(Set<AutoavaliacaoEixos> autoavaliacaoEixos) {
+        this.autoavaliacaoEixos = autoavaliacaoEixos;
+    }
+
+    public List<Indicador> getIndicadores() {
+        return indicadores;
+    }
+
+    public void setIndicadores(List<Indicador> indicadores) {
+        this.indicadores = indicadores;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
