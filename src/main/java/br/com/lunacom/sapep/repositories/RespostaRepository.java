c7c0160d4746eb7568fd02866ed0ab2eb0cbae60
alexpereira2017 <alexpereira2004@gmail.com>
2019-08-11 23:50:01 -0300

    [#009] REST Resposta (Start)

---
+++ src/main/java/br/com/lunacom/sapep/repositories/RespostaRepository.java
@@ -0,0 +1,6 @@
+package br.com.lunacom.sapep.repositories;
+
+import br.com.lunacom.sapep.domain.Resposta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespostaRepository extends JpaRepository<Resposta, Integer> { }
