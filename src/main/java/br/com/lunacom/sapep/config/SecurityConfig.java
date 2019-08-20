52ae71d17a5f3982e8ea57a3244683800a312acd
alexpereira2017 <alexpereira2004@gmail.com>
2019-08-19 23:36:15 -0300

    [#010] Spring Security (Done)  Implementando autorização

--- src/main/java/br/com/lunacom/sapep/config/SecurityConfig.java
+++ src/main/java/br/com/lunacom/sapep/config/SecurityConfig.java
@@ -37,24 +37,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JWTUtil jwtUtil;
 
-    private static final String[] PUBLIC_MATCHERS = {
+    private static final String[] PUBLIC_MATCHERS_COMMONS = {
         "/h2-console/**"
     };
 
-    private static final String[] PUBLIC_MATCHERS_GET = {
+    private static final String[] PUBLIC_MATCHERS = {
         "/curso/**",
-        "/usuario/**"
-    };
-
-    private static final String[] PUBLIC_MATCHERS_POST = {
-        "/curso/**",
-//        "/conceito/**",
-//        "/responsavel/**",
-//        "/autoavaliacao/**",
-//        "/eixo/**",
-//        "/indicador/**",
-//        "/resposta/**",
-//        "/usuario/**",
+        "/conceito/**",
+        "/responsavel/**",
+        "/usuario/**",
+        "/autoavaliacao/**",
+        "/eixo/**",
+        "/indicador/**",
+        "/resposta/**",
     };
 
     @Override
@@ -66,8 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+//                .antMatchers(PUBLIC_MATCHERS).permitAll()
+//                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
