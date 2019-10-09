c32cebdd4fc3caf2792fe8797d3a9670e30c77c0
alexpereira2017 <alexpereira2004@gmail.com>
2019-10-08 22:41:40 -0300

    Habilitar permissão de acesso ao banco H2

--- src/main/java/br/com/lunacom/sapep/config/SecurityConfig.java
+++ src/main/java/br/com/lunacom/sapep/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-//                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .antMatchers(PUBLIC_MATCHERS_COMMONS).permitAll()
 //                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
