8168cf818d82902a4aa1d55c53d04bc58a19481b
alexpereira2017 <alexpereira2004@gmail.com>
2019-10-23 15:40:25 -0300

    [#019] Criar Ações > Lançamentos - Salvando os dados do form

---
+++ src/main/java/br/com/lunacom/sapep/domain/dto/EixoDTO.java
@@ -0,0 +1,18 @@
+package br.com.lunacom.sapep.domain.dto;
+
+import br.com.lunacom.sapep.domain.Indicador;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class EixoDTO {
+
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private Integer ordem;
+    private List<Indicador> indicadores;
+}
