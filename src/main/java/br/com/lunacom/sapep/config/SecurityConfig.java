0c8872a5f05f25249bb7ae606de2ea4f58336e1b
alexpereira2017 <alexpereira2004@gmail.com>
2019-12-16 23:09:16 -0300

    Swagger

--- src/main/java/br/com/lunacom/sapep/config/SecurityConfig.java
+++ src/main/java/br/com/lunacom/sapep/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -53,6 +54,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS_POST = {
     };
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                    "/configuration/ui",
+                                    "/swagger-resources/**",
+                                    "/configuration/**",
+                                    "/swagger-ui.html",
+                                    "/webjars/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
